f9a66f21e6d89d7834506df7b2d7a8a13f5a8e5f
Justin Thompson <justindthomps@gmail.com>
2022-01-29 16:07:39 -0600

    added basic input screen for budget.

---
+++ FTApp/FTApp/GetBudgetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GetBudgetViewController.swift
+//  FTApp
+//
+//  Created by Justin Thompson on 1/29/22.
+//
+
+import UIKit
+
+class GetBudgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Set Budget"
+    }
+
+
+}
