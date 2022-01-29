4f0b7915d7630bd4e0770847f90d813073e23664
mcsandovall <rasec_oiram@live.com.mx>
2022-01-29 16:23:48 -0600

    user info class developed

--- FTApp/FTApp/Interface.swift
+++ FTApp/FTApp/Interface.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct financial_info {
-    var income : Int = get_income();
+    var income : Int = 0;
     var rent : Int = 0;
     var utilities : Int = 0;
     var groceries : Int = 0;
@@ -20,12 +20,39 @@ struct financial_info {
 class user_info{
     // implement the 50/30/20 rule
     
-    var expected_essential : Int = 0;
-    var acutal_essentials : Int = 0;
+    // var of expected essentials (perfect world scenario)
+    var expected_essential : Int;
+    var expected_non_essentials : Int;
+    var expected_savings : Int;
     
-}
-
-func get_income() -> Int{
+    // vars of actual calculations, from the user
+    var acutal_essentials : Int;
+    var acutal_non_essentils : Int;
+    var actual_savings : Int;
     
-    return 0;
+    init(info : financial_info){
+        // work with expected variables
+        expected_essential = Int( Double(info.income) * 0.5 );
+        expected_non_essentials = Int( Double(info.income) * 0.3);
+        expected_savings = Int ( Double(info.income) * 0.2);
+        
+        //actual values
+        let sum : Int = info.rent + info.utilities + info.groceries + info.dinning + info.entretainment + info.personal_expenses;
+        
+        acutal_essentials = Int (Double( (info.rent + info.utilities + info.groceries) / sum));
+        acutal_non_essentils = Int ( Double( (info.dinning + info.entretainment + info.personal_expenses) / sum));
+        actual_savings = info.income - (acutal_essentials + acutal_non_essentils);
+    }
+    
+    func get_essential_differnece() -> Int{
+        return acutal_essentials - expected_essential;
+    }
+    
+    func get_non_essential_difference() -> Int{
+        return acutal_non_essentils - expected_non_essentials;
+    }
+    
+    func get_savings_difference() -> Int{
+        return expected_savings - expected_savings;
+    }
 }
